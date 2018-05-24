ec9a53a00732cf585764de83078fc1cb39f4da17
lijiafu000 <lijiafu000@lijiafudeMacmini.local>
2018-05-24 09:20:53 +0800

    update directory of Application/Admin/View/

--- Application/Admin/Model/AdminModel.class.php
+++ Application/Admin/Model/AdminModel.class.php
@@ -3,12 +3,35 @@
 
   use Think\Model;
   
-  class AdminModel extends Model {
-    protected $_validate = array(
-      array('username', 'require', '管理员名称不能为空!', 1),
-      array('password', 'require', '管理员密码不能为空!', 1, regex, 1),
-      array('username', '', '管理员名称已经存在!', 1, 'unique', 1),
-      array('username', '', '管理员名称已经存在!', 1, 'unique', 2)
-    );
+  class AdminModel extends Model
+  {
+      protected $_validate = array(
+        array('username', 'require', '管理员名称不能为空!', 1),
+        array('password', 'require', '管理员密码不能为空!', 1, regex, 1),
+        array('username', '', '管理员名称已经存在!', 1, 'unique', 1),
+        array('username', '', '管理员名称已经存在!', 1, 'unique', 2),
+        array('verify', 'verify', '验证码不正确!', 1, 'callback', 4)
+      );
+
+      public function login()
+      {
+          $password = $this->password;
+          $info = $this->where("username='$this->username'")->find();
+          if($info) {
+              if($info['password'] == md5($password)) {
+                  return true;
+              } else {
+                  return false;
+              }
+          } else {
+              return false;
+          }
+      }
+
+      public function verify($code)
+      {
+          $verify = new \Think\Verify();
+          return $verify->check($code, '');
+      }
   }
 ?>
